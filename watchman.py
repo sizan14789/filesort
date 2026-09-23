45d24c5e1a2b34d75a489ca13069f41ca5c7a6a6
sizan14789 <sizanalt@gmail.com>
2026-09-23 13:09:39 +0600

    EventHandler implemented

--- watchman.py
+++ watchman.py
@@ -1,13 +1,15 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from setup import BASE
+from main_helper import handle_file
+from pathlib import Path
 
 class EventHandler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return
         
-        print("voala")
+        handle_file(Path(event.src_path))
 
 watchman = Observer()
 watchman.schedule(EventHandler(), str(BASE), recursive=False)
